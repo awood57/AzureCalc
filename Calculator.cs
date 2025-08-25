7b70ed291e22017a911ebf4fafdce8f460129c93
spike <spike@debian>
2025-08-25 15:22:27 +0100

    Added basic calculator functions

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+namespace Math
+{
+	public class Calculator
+	{
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Sub(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multi(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Div(double a, double b)
+		{
+			if (b == 0)
+				throw new DivideByZeroException("Divided by zero!");
+
+			return a / b;
+		}
+	}
+}
