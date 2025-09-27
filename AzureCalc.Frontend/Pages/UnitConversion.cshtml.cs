5c5c1d6934b5f314f0d1bc6e897b81821bf163c9
spike <spike@debian>
2025-09-27 17:25:31 +0100

    Some basic structure for unit conversion (distance)

--- AzureCalc.Frontend/Pages/UnitConversion.cshtml.cs
+++ AzureCalc.Frontend/Pages/UnitConversion.cshtml.cs
@@ -7,5 +7,33 @@ namespace AzureCalc.Frontend.Pages
 {
 	public class UnitConversionModel : PageModel
 	{
+		private readonly UnitConversion _unitConversion = new UnitConversion();
+		// UnitConversionStorage goes here
+		
+		// Conversion variables
+		[BindProperty]
+		public double Num { get; set; }
+		
+		[BindProperty]
+		public string Distance { get; set; } = string.Empty;
+
+		public double? Result { get; set; }
+
+		public void OnPost()
+		{
+			if(!string.IsNullOrEmpty(Distance))
+			{
+				switch(Distance)
+				{
+					case "ft":
+						break;
+					case "yards":
+						break;
+					case "km":
+						break;
+				}
+			}
+			// Other conversions go here
+		}
 	}
 }
