ef92084f5f60f82d3433fbfee6c8412cfa0f95be
spike <awood57@protonmail.com>
2025-11-26 17:44:29 +0000

    Refactored history API

--- AzureCalc.Api/HistoryFunction.cs
+++ AzureCalc.Api/HistoryFunction.cs
@@ -1,23 +1,47 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+using System.Text.Json;
+using AzureCalc.Logic;
+using AzureCalc.Services;
 
 namespace AzureCalc.Api;
 
 public class HistoryFunction
 {
     private readonly ILogger<HistoryFunction> _logger;
+    private readonly CalculationStorage _calcStorage;
+    private readonly ConversionStorage _convStorage;
 
-    public HistoryFunction(ILogger<HistoryFunction> logger)
+    public HistoryFunction(ILogger<HistoryFunction> logger, CalculationStorage calcStorage, ConversionStorage convStorage)
     {
         _logger = logger;
+	_calcStorage = calcStorage;
+	_convStorage = convStorage;
     }
 
+    // Return all calculations and conversions from storage
     [Function("HistoryFunction")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "history/all")] HttpRequestData request)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        _logger.LogInformation("Fetching full history of calculations and conversions.");
+
+	// Fetch histories
+	var calcTask = _calcStorage.GetAllAsync();
+	var convTask = _convStorage.GetAllAsync();
+	await Task.WhenAll(calcTask, convTask);
+
+	var responseObj = new
+	{
+		calculations = await calcTask,
+		conversions = await convTask
+	};
+
+	// Create response and write json
+	var response = request.CreateResponse(HttpStatusCode.OK);
+	await response.WriteAsJsonAsync(responseObj);
+
+	return response;
     }
 }
